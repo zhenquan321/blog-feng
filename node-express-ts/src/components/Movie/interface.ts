9e0cc9f10c2c3c954a6ae357b7afe74a57b3f421
fengzhenquan <616447247@qq.com>
2019-11-05 18:33:45 +0800

    修改文件目录

---
+++ node-express-ts/src/components/Movie/interface.ts
@@ -0,0 +1,15 @@
+import { IMovieModel } from './model';
+
+/**
+ * @export
+ * @interaface MovieReptileServiceInterface
+ */
+export interface MovieService {
+    insert(MovieModel: IMovieModel): Promise < IMovieModel > ;
+    remove(id: string): Promise < IMovieModel > ;
+    findOne(id: string): Promise<IMovieModel|false>;
+    findAll(pageQurey?: any): Promise<IMovieModel[]>;
+    findOneByName(name: string): Promise<IMovieModel>;
+    getCount(): Promise<number>;
+    update(qurey:any,body: any): Promise<void>;
+}
---
+++ node-express-ts/src/components/Movie/service.ts
@@ -0,0 +1,159 @@
+import * as Joi from 'joi';
+import MovieModel, { IMovieModel } from './model';
+import { MovieService } from './interface';
+import { Types } from 'mongoose';
+
+/**
+ * @export
+ * @implements {IMovieModelService}
+ */
+const MovieService: MovieService = {
+
+
+    /**
+     * @param {IMovieModel} user
+     * @returns {Promise < IMovieModel >}
+     * @memberof UserService
+     */
+
+    async insert(body: IMovieModel): Promise<IMovieModel> {
+        try {
+            // const validate: Joi.ValidationResult < MovieModel > = UserValidation.createUser(body);
+            // if (validate.error) {
+            //     throw new Error(validate.error.message);
+            // }
+            const hsmovie: IMovieModel = await this.findOneByName(body.name);
+            if (hsmovie && hsmovie.name) {
+                // console.log('该电影已存在');
+                const Movie: IMovieModel = await MovieModel.update({ name: body.name }, body);
+            } else {
+                const Movie: IMovieModel = await MovieModel.create(body);
+
+                return Movie;
+            }
+
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+    async findOneByName(name: string): Promise<IMovieModel> {
+        return await MovieModel.findOne({
+            name
+        });
+    },
+    /*
+     * @param {string} id
+     * @returns {Promise < IMovieModel >}
+     * @memberof UserService
+     */
+    async findOne(id: string): Promise<IMovieModel|false> {
+        try {
+            // const validate: Joi.ValidationResult<{
+            //     id: string
+            // }> = UserValidation.getUser({
+            //     id
+            // });
+
+            // if (validate.error) {
+            //     throw new Error(validate.error.message);
+            // }
+            let Movie: any = await MovieModel.findOne({
+                _id: Types.ObjectId(id)
+            });
+            if (Movie) {
+
+                return Movie;
+
+            } 
+            return false;
+
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    /**
+        * @returns {Promise < IMovieModel[] >}
+        * @memberof UserService
+        */
+    async findAll(pageQurey?: any): Promise<any> {
+
+        const page: number = pageQurey && pageQurey.page ? Number(pageQurey.page) : 0;
+        const pagesize: number = pageQurey && pageQurey.pagesize ? Number(pageQurey.pagesize) : 20;
+        try {
+            const findKeyObj: any = {
+                downLink: { $ne: '' },
+                imgUrl: { $ne: '' },
+            };
+
+
+            if (pageQurey && pageQurey.year) {
+                findKeyObj.years = Number(pageQurey.year);
+            }
+            if (pageQurey && pageQurey.type) {
+                findKeyObj.type = pageQurey.type;
+            }
+
+            if (pageQurey && pageQurey.keyword) {
+                findKeyObj.name = { $regex: pageQurey.keyword, $options: 'i' };
+            }
+
+            console.log(findKeyObj);
+            const movieList: IMovieModel[] = await MovieModel.find(findKeyObj).limit(pagesize).skip(page * pagesize);
+            const count: number = await MovieModel.find(findKeyObj).countDocuments();
+
+            return {
+                count,
+                data: movieList,
+            };
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+
+    async update(qurey: any, body: any): Promise<void> {
+        try {
+            const updateInfo: any = await MovieModel.updateOne(qurey, { $set: body });
+            console.log(updateInfo);
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+    async getCount(): Promise<number> {
+        try {
+            return await MovieModel.find().countDocuments();
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    },
+
+
+    /**
+     * @param {string} id
+     * @returns {Promise < IMovieModel >}
+     * @memberof UserService
+     */
+    async remove(id: string): Promise<IMovieModel> {
+        try {
+            // const validate: Joi.ValidationResult<{
+            //     id: string
+            // }> = UserValidation.removeUser({
+            //     id
+            // });
+
+            // if (validate.error) {
+            //     throw new Error(validate.error.message);
+            // }
+
+            const user: IMovieModel = await MovieModel.findOneAndRemove({
+                _id: Types.ObjectId(id)
+            });
+
+            return user;
+        } catch (error) {
+            throw new Error(error.message);
+        }
+    }
+};
+
+export default MovieService;
