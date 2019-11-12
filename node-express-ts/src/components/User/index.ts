533f1eb8d72a7745525a01924e6139797257115d
fengzhenquan <616447247@qq.com>
2019-11-12 21:43:15 +0800

    服务端代码合并

--- node-express-ts/src/components/User/index.ts
+++ node-express-ts/src/components/User/index.ts
@@ -97,8 +97,11 @@ export async function update(req: Request, res: Response, next: NextFunction): P
         const shDataNei: any = await client.textCensorUserDefined(userInfoSh);
 
         if ((!shData) || shData.error_msg || (shData.result && shData.result[0] && shData.result[0].res_code === 0)) {
+
             console.log(shData, '头像审核通过');
+
         } else {
+
             res.status(200).json({
                 state: 1,
                 msg: '头像违规,请再次上传',
