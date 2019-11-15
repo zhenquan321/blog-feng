aa212c74146540c9df732385474d3edc5f2adc5a
fengzhenquan <616447247@qq.com>
2019-11-15 21:22:39 +0800

    优化https配置

--- node-express-ts/src/config/server/index.ts
+++ node-express-ts/src/config/server/index.ts
@@ -4,27 +4,29 @@ import * as fs from 'fs';
 import * as serverHandlers from './serverHandlers';
 import server from './server';
 
-
 const httpsOption = {
     key : fs.readFileSync("./https/3094015_lmongo.com.key"),
     cert: fs.readFileSync("./https/3094015_lmongo.com.pem")
 }
 
-
 const Server: http.Server = http.createServer(server);
+const ServerHttps: https.Server = https.createServer(httpsOption,server);
 
 /**
  * Binds and listens for connections on the specified host
  */
+
 Server.listen(server.get('port'));
-https.createServer(httpsOption, server).listen(443);
+ServerHttps.listen(443);
 
 /**
  * Server Events
  */
-Server.on('error',
-    (error: Error) => serverHandlers.onError(error, server.get('port')));
-Server.on('listening',
-    serverHandlers.onListening.bind(Server));
+
+Server.on('error',(error: Error) => serverHandlers.onError(error, server.get('port')));
+Server.on('listening',serverHandlers.onListening.bind(Server));
+
+ServerHttps.on('error',(error: Error) => serverHandlers.onError(error,443));
+ServerHttps.on('listening',serverHandlers.onListening.bind(ServerHttps));
 
     
