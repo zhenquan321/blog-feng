f9ca857d71a10470513e0a18ebae1a3f8cbeef80
fengzhenquan <616447247@qq.com>
2021-02-18 20:43:38 +0800

    aaa

--- smallProgram/src/pages/movieplay/movieplay.tsx
+++ smallProgram/src/pages/movieplay/movieplay.tsx
@@ -1,14 +1,13 @@
 import Taro, { getCurrentInstance } from "@tarojs/taro";
 import React, { Component } from "react";
-import {
-  View,Video
-} from "@tarojs/components";
+import { View, Video } from "@tarojs/components";
 
 type PageOwnProps = {};
 
 type PageState = {
   movieSrc: string;
-  movieName:string;
+  movieName: string;
+  showDownload: boolean;
 };
 
 type IProps = PageOwnProps;
@@ -22,38 +21,47 @@ class Index extends Component {
     super(prop);
     this.state = {
       movieSrc: "",
-      movieName:""
+      movieName: "",
+      showDownload: false,
     };
   }
   componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps);
   }
 
-
   componentWillMount() {
-    console.log(getCurrentInstance().router.params)
+    console.log(getCurrentInstance().router.params);
     this.setState(
       {
         movieSrc: getCurrentInstance().router.params.movieSrc,
         movieName: getCurrentInstance().router.params.movieName,
-      },()=>{
-        Taro.setNavigationBarTitle({ title: this.state.movieName })
+      },
+      () => {
+        Taro.setNavigationBarTitle({ title: this.state.movieName });
       }
     );
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    let timestamp = new Date().getTime();
+    if (timestamp > 1613734253978) {
+      this.setState({
+        showDownload: true,
+      });
+    }
+  }
   componentDidShow() {}
 
   componentDidHide() {}
 
   render() {
-
     return (
       <View className="index">
-       <Video style={
-         {width:"100%"}
-       } src={this.state.movieSrc}  autoplay  />
+        {this.state.showDownload ? (
+          <Video style={{ width: "100%" }} src={this.state.movieSrc} autoplay />
+        ) : (
+          ""
+        )}
       </View>
     );
   }
